75924ad890ac99197cb5a5f8125fe712f64c0598
Blashor <blakemt4@gmail.com>
2026-04-09 14:26:00 -0700

    Add files via upload

--- INSIDE_pipeline/run_INSIDE.sh
+++ INSIDE_pipeline/run_INSIDE.sh
@@ -23,11 +23,16 @@ cd /home/bmt26/garg/basenji/basenji-master/bin
 
  
 ### Install Info
+
 #orange for basenji_data there is an issue with chrY that needs to be removed from fasta files 
 #orange also for this, it calls two other python files basenji_data (_read and _write), need to put python in front of this
 #red for basenji_train need to do this: mamba install -c conda-forge cudatoolkit cudnn
 #yellow changed limit behavior to bedtools intersect -u for unique hits that don't fully overlap
-#enh center margin
+
+#json setup files should be placed in /home/bmt26/garg/basenji/basenji-master/json
+
+###
+
 python /home/bmt26/scripts/INSIDE/INSIDE.py cluster140
 #python /home/bmt26/scripts/INSIDE/INSIDE_score_parents_no_selection.py cluster140
 #python /home/bmt26/scripts/INSIDE/INSIDE_within_genome.py 1000o14 
